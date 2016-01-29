df1b2a70bba6c436f5d6f669f8ab201b56fc25a3
Bill Broughton <bill@gotechspring.com>
2016-01-28 21:11:59 -0500

    Actually use the error function

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -13,7 +13,7 @@ Template.postSubmit.events({
             }
 
             if (result.postExists) {
-                alert('This link has already been posted');
+                throwError('This link has already been posted');
             }
 
             Router.go('postPage', {_id: result._id});
